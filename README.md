ae2d221eb01f2d1a65570801fddc232d4101eb61
joahlenzo <100421539+joahlenzo@users.noreply.github.com>
2025-05-06 14:56:25 +0200

    te

---
+++ README.md
@@ -0,0 +1,6 @@
+Hier machen wir ein Dockerfile für eine kleine Webapplikation.
+
+1. Dockerbuild (gemäss Screenshot)
+2. Docker run (Port abgeändert dass sicher nicht zwei mal der Port 3001 verwendet wird)
+3. Funktioniert
+
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('(-: Hello FS2025 DevOps Course! :-)');
+  res.send('(-: Hello Joah Lenzo - here your first Webapplication with node.js and build with Dockerfile :-)');
 });
 
 app.listen(PORT, HOST);
