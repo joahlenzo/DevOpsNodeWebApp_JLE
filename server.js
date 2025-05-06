8d57e40770803c826e8ff719062ad288bb41efba
joahlenzo <100421539+joahlenzo@users.noreply.github.com>
2025-05-06 15:56:00 +0200

    Update Server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('(-: Hello Joah Lenzo - here your first Webapplication with node.js and build with Dockerfile :-) Complete pipeline now');
+  res.send('(-: Hello Joah Lenzo - here your first Webapplication with node.js and build with Dockerfile :-) Complete pipeline now - Pipeline is running automatically! -:)\n');
 });
 
 app.listen(PORT, HOST);
